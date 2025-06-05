d09ac556a534e130a396f45a3d5b770c2b312a8a
Saiganesh Kudipudi <Saiganesh.Kudipudi@zeuslearning.mumbai>
2025-06-05 12:17:11 +0530

    css change

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ const femaleRadio = document.getElementById("female");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", (event) => {
-    event.preventDefault();
-
     if(fname.value === ""){
-        alert("Enter your name");
         return;
     }
     if(comment.value === ""){
-        alert("Enter a comment");
         return;
     }
     if(!maleRadio.checked && !femaleRadio.checked){
-        alert("Select a gender");
         return;
     }
     console.log(`
